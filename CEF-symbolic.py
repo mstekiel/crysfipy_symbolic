cec58ed32a929bcbaca182ece4f784a79ee7f474
mstekiel <64463131+mstekiel@users.noreply.github.com>
2023-03-24 16:50:39 +0100

    Update Hamiltonian construction

--- CEF-symbolic.py
+++ CEF-symbolic.py
@@ -6,54 +6,66 @@ from timeit import default_timer as timer
 from cefmatrices import *
 
 sympy.init_printing(use_unicode=False)
-B20, B22, B2m2 = sympy.symbols('B20, B22, B2m2')
-B40, B42, B4m2, B43, B4m3, B44, B4m4 = sympy.symbols('B40, B42, B4m2, B43, B4m3, B44, B4m4')
-B60, B62, B6m2, B63, B6m3, B64, B6m4, B66, B6m6 = sympy.symbols('B60, B62, B6m2, B63, B6m3, B64, B6m4, B66, B6m6')
+B20, B22, B2m2 = sympy.symbols('B20, B22, B2m2', real=True)
+B40, B42, B4m2, B43, B4m3, B44, B4m4 = sympy.symbols('B40, B42, B4m2, B43, B4m3, B44, B4m4', real=True)
+B60, B62, B6m2, B63, B6m3, B64, B6m4, B66, B6m6 = sympy.symbols('B60, B62, B6m2, B63, B6m3, B64, B6m4, B66, B6m6', real=True)
 
 
 # Dont forget to edit these fields
-Jval = sympy.Rational(3,2)
-lattice='hexagonal_1' #can vary
+Jval = 4
+lattice='cubic_2'
+
+
+###
+# Calculations
 
 Jvalstr = f'{Jval:.1f}'.replace('.','p')
-filenameprefix = f'./Symbolic-output/J_{Jvalstr}_lattice'
+filenameprefix = f'./Symbolic-output/J_{Jvalstr}_{lattice}'
 
 # Symmetry allowed Bnm according to McPhase manual
 # https://www2.cpfs.mpg.de/~rotter/homepage_mcphase/manual/node133.html
-match lattice:
-#	case 'triclinic':
-#		H = sympy.Matrix(B20 * O_20(Jval) + B22 * O_22(Jval) + B40 * O_40(Jval) + B42 * O_42(Jval) + B43 * O_43(Jval) + B44 * O_44(Jval) + B60 * O_60(Jval) + B62 * O_62(Jval) + B63 * O_63(Jval) + B64 * O_64(Jval) + B66 * O_66(Jval))
-	case 'monoclinic':
-		H = sympy.Matrix(B20 * O_20(Jval) + B22 * O_22(Jval) + B2m2 * O_2m2(Jval) + \
-				 B40 * O_40(Jval) + B42 * O_42(Jval) +  B4m2 * O_4m2(Jval) + B44 * O_44(Jval) + B4m4 * O_4m4(Jval) + \
-				 B60 * O_60(Jval) + B62 * O_62(Jval) + B6m2 * O_6m2(Jval) + B64 * O_64(Jval) + B6m4 * O_6m4(Jval) + B66 * O_66(Jval) + B6m6 * O_6m6(Jval))
-	case 'rhombic':
-		H = sympy.Matrix(B20 * O_20(Jval) + B22 * O_22(Jval) + \
-				 B40 * O_40(Jval) + B42 * O_42(Jval) + B44 * O_44(Jval) + \
-				 B60 * O_60(Jval) + B62 * O_62(Jval) + B64 * O_64(Jval) + B66 * O_66(Jval))
-	case 'tetragonal_1':
-		H = sympy.Matrix(B20 * O_20(Jval) + \
-				 B40 * O_40(Jval) + B44 * O_44(Jval) + B4m4 * O_4m4(Jval) + \
-				 B60 * O_60(Jval) + B64 * O_64(Jval) + B6m4 * O_6m4(Jval))
-	case 'tetragonal_2':
-		H = sympy.Matrix(B20 * O_20(Jval) + B40 * O_40(Jval) + B44 * O_44(Jval) + B60 * O_60(Jval) + B64 * O_64(Jval))
-	case 'trigonal_1':
-		H = sympy.Matrix(B20 * O_20(Jval) + \
-				 B40 * O_40(Jval) + B43 * O_43(Jval) + + B4m3 * O_4m3(Jval) + \
-				 B60 * O_60(Jval) + B63 * O_63(Jval) + B6m3 * O_6m3(Jval) + B66 * O_66(Jval) + B6m6 * O_6m6(Jval))
-	case 'trigonal_2':
-		H = sympy.Matrix(B20 * O_20(Jval) + B40 * O_40(Jval) + B43 * O_43(Jval) + B60 * O_60(Jval) + B63 * O_63(Jval) + B66 * O_66(Jval))
-	case 'hexagonal_1':
-		H = sympy.Matrix(B20 * O_20(Jval) + B40 * O_40(Jval) + B60 * O_60(Jval) + B66 * O_66(Jval) + B6m6 * O_6m6(Jval))
-	case 'hexagonal_2':
-		H = sympy.Matrix(B20 * O_20(Jval) + B40 * O_40(Jval) + B60 * O_60(Jval) + B66 * O_66(Jval))
-	case 'cubic_1':
-		H = sympy.Matrix(B40 * O_40(Jval) + sympy.Rational(5,2) * B40 * O_40(Jval) + B60 * O_60(Jval) + -1 * B66 * O_66(Jval) + sympy.Rational(-21, 2) * B60 * O_60(Jval) + B66 * O_66(Jval))
-	case 'cubic_2':
-		H = sympy.Matrix(B40 * O_40(Jval) + sympy.Rational(5,2) * B40 * O_40(Jval) + B60 * O_60(Jval) + sympy.Rational(-21, 2) * B60 * O_60(Jval))
+H = sympy.Matrix({
+'monoclinic':
+    B20*O_20(Jval) + B22*O_22(Jval) + B2m2*O_2m2(Jval) + \
+    B40*O_40(Jval) + B42*O_42(Jval) + B4m2*O_4m2(Jval) + B44*O_44(Jval) + B4m4*O_4m4(Jval) + \
+    B60*O_60(Jval) + B62*O_62(Jval) + B6m2*O_6m2(Jval) + \
+    B64*O_64(Jval) + B6m4*O_6m4(Jval) + B66*O_66(Jval) + B6m6*O_6m6(Jval),
+'rhombic':
+    B20*O_20(Jval) + B22*O_22(Jval) + \
+    B40*O_40(Jval) + B42*O_42(Jval) + B44*O_44(Jval) + \
+    B60*O_60(Jval) + B62*O_62(Jval) + B64*O_64(Jval) + B66*O_66(Jval),
+'tetragonal_1':
+    B20*O_20(Jval) + \
+    B40*O_40(Jval) + B44*O_44(Jval) + B4m4*O_4m4(Jval) + \
+    B60*O_60(Jval) + B64*O_64(Jval) + B6m4*O_6m4(Jval),
+'tetragonal_2':
+    B20*O_20(Jval) + B40*O_40(Jval) + B44*O_44(Jval) + \
+    B60*O_60(Jval) + B64*O_64(Jval),
+'trigonal_1':
+    B20*O_20(Jval) + \
+    B40*O_40(Jval) + B43*O_43(Jval) + B4m3*O_4m3(Jval) + \
+    B60*O_60(Jval) + B63*O_63(Jval) + B6m3*O_6m3(Jval) + B66*O_66(Jval) + B6m6*O_6m6(Jval),
+'trigonal_2':
+    B20*O_20(Jval) + B40*O_40(Jval) + B43*O_43(Jval) + \
+    B60*O_60(Jval) + B63*O_63(Jval) + B66*O_66(Jval),
+'hexagonal_1':
+    B20*O_20(Jval) + B40*O_40(Jval) + \
+    B60*O_60(Jval) + B66*O_66(Jval) + B6m6*O_6m6(Jval),
+'hexagonal_2':
+    B20*O_20(Jval) + B40*O_40(Jval) + B60*O_60(Jval) + B66*O_66(Jval),
+'cubic_1':
+    B40*O_40(Jval) + sympy.Rational(5,2)*B40*O_44(Jval) + \
+    B60*O_60(Jval) + -1*B66*O_62(Jval) + sympy.Rational(-21, 2)*B60*O_64(Jval) + B66*O_66(Jval),
+'cubic_2':
+    B40*O_40(Jval) + sympy.Rational(5,2)*B40*O_44(Jval) + \
+    B60*O_60(Jval) + sympy.Rational(-21, 2)*B60*O_64(Jval)
+}[lattice])
 
-print( H )
-
+sympy.pprint( H )
+H_dag = sympy.transpose(sympy.conjugate(H))
+if not H==H_dag:
+    sympy.pprint(H_dag)
+    raise ValueError(f'Hamiltonian is not hermitian')
 
 diagonalize = True
 exportLatex = False
@@ -133,8 +145,10 @@ if exportSympy:
             ff.write(f'{Ps[:,it]}\n')
             ff.write(f'\n')
             
+        ff.write(f'Diagonalized matrix D from H=U_dag D U\n')
         ff.write(f'{Ds}')
         ff.write(f'\n')
+        ff.write(f'Rotation matrix U from H=U_dag D U\n')
         ff.write(f'{Ps}')
             
         
