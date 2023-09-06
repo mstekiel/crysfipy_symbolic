36fbdf9a628402a6bcdfca9ff5fca8d0ce4de6d2
mstekiel <64463131+mstekiel@users.noreply.github.com>
2023-09-06 16:55:14 +0200

    Eigenvalues/vectors

--- cef_hamiltonian.py
+++ cef_hamiltonian.py
@@ -1,10 +1,13 @@
 import sympy
 import numpy as np
 from copy import deepcopy
+import warnings
 
-from typing import List, Dict
+from typing import List, Dict, Tuple
 from sympy.matrices import Matrix, ShapeError
+
 from cef_matrices import *
+from data_containers import sympy_expression_mapping
 
 from sympy import pretty
 
@@ -18,9 +21,22 @@ class Hamiltonian():
         Base of the Hamiltonian.
     matrix: sympy.Matrix
         Matrix representation of the hamiltonian in `self.base`
+    eigenvalues: List[Tuple[expr, int]]
+        Lsit of eigenvalues, where each entry contains
+        eigenvalue's formula and degeneracy.
+    eigenvectors: Lsit[List[expr]]
+        List of eigenvectors in order of `eigenvalues`.
+
     '''
 
     def __init__(self, base: List[str], matrix: Matrix):
+        '''
+        Class constructor.
+
+        Requires `base` and `matrix` fields to fill, sets others to `None`.
+        Checks the dimensionality of the Hamiltonian by ensuring equal dimensions 
+        of `base` and `matrix`.
+        '''
         if not matrix.shape[0] == matrix.shape[1]:
             raise ShapeError(f'Hamiltonian matrix is not square. Defined dimensions={matrix.shape}')
         
@@ -30,8 +46,10 @@ class Hamiltonian():
         self.base = base
         self.matrix = matrix
 
-        self.eigenvalues = None
-        self.eigenvectors = None
+        self.eigenvalues = []
+        self.eigenvectors = []
+
+        self._translate_table = sympy_expression_mapping()
 
     def __str__(self):
         ret = 'Hamiltonian base\n'
@@ -41,13 +59,111 @@ class Hamiltonian():
 
         return ret
     
-    def cal_eigenvalues(self):
+    def _evaluate_str(self, formula: str, symbol_values: Dict[str, float]) -> float:
+        '''
+        Helper function to evaluate the numerical value 
+        of a given formula
+        '''
+
+        translate_table = symbol_values
+        translate_table.update(self._translate_table)
+
+        return eval(formula, translate_table)
+    
+    def Neval_eigenvalues(self, symbol_values: Dict[str, float], shift_to_zero: bool=True, precision: int=10) -> List[Tuple[float, int]]:
+        '''
+        Numerically evaluate eigenvalues based on the `symbol values`
+        substitution table. Will cast the eigenvalue to real numbers.
+
+        Reises
+        ------
+
+        ValueError
+            When the imaginary part of the eigenvalue is higher than 10^(-precision)
+        '''
+        res = []
+        for formula, degeneracy in self.eigenvalues:
+            value = np.around(self._evaluate_str(str(formula), symbol_values), precision)
+
+            if np.abs(np.imag(value)) > 0:
+                raise ValueError(f'Imaginary frequency {value}')
+            
+            res.append( (np.real(value), degeneracy) )
+
+        if shift_to_zero:
+            minE = min([E for E, _ in res])
+            res = [(E-minE, deg) for E, deg in res]
+
+        return res
+    
+    def Neval_eigenvectors(self, symbol_values: Dict[str, float], normalize: bool=False, precision: bool=10) -> List[List[float]]:
+        '''
+        Numerically evaluate eigenvectors based on the `symbol values`
+        substitution table. Will cast the eigenvalue to real numbers.
+
+        Reises
+        ------
+
+        ValueError
+            When the imaginary part of the eigenvalue is higher than 10^(-precision)
+        '''
+        res = []
+        for eigenspace in self.eigenvectors:
+            eigenspace_num = []
+            for eigenvector in eigenspace:
+                ev_num = np.array([self._evaluate_str(str(x), symbol_values) for x in eigenvector], dtype=np.complex128)
+                
+                # Trim values
+                ev_num = np.around(ev_num, precision)
+                # Get rid of imag if smaller than precision
+                if np.all( np.abs(np.imag(ev_num)) < np.power(1.0, -precision)):
+                    ev_num = np.real(ev_num)
+
+                if normalize:
+                    ev_num /= np.linalg.norm(ev_num)
+
+                eigenspace_num.append(ev_num)
+
+            res.append(eigenspace_num)
+
+        return res
+    
+    def determine_eigenvalues(self):
         '''
         Determine the eigenvalues of the Hamiltonian by constructing the
         characteristic polynomial and finding its roots.
         '''
-        self.eigenvalues = sympy.roots(self.matrix.charpoly())
+        if self.eigenvalues:
+            warnings.warn(f'Eigenvalues were already calculated. Repeating calculation.', UserWarning)
+            self.eigenvalues = []    
+
+        for eval_formula, eval_deg in sympy.roots(self.matrix.charpoly()).items():
+            self.eigenvalues.append((eval_formula, eval_deg))
+
         return self.eigenvalues
+    
+    def determine_eigenvectors(self):
+        '''
+        Determine the eigenvalues of the Hamiltonian by constructing the
+        characteristic polynomial and finding its roots.
+        '''
+        if self.eigenvectors:
+            warnings.warn(f'Eigenvectors were already calculated. Repeating calculation.', UserWarning)
+            self.eigenvectors = []
+
+        if not self.eigenvalues:
+            warnings.warn(f'Eigenvalues not calculated in advance. Calculating eigenvalues first.')
+            self.determine_eigenvalues()
+
+        for eigenvalue, degeneracy in self.eigenvalues:
+            eigenvectors = (self.matrix - eigenvalue*sympy.eye(self.matrix.rank())).nullspace()
+
+            self.eigenvectors.append(eigenvectors)
+
+        # for eval_formula, eval_deg in sympy.roots(self.matrix.charpoly()).items():
+        #     self.eigenvalues.append((eval_formula, eval_deg))
+
+        return self.eigenvectors
 
     def sort_base(self):
         '''
@@ -68,7 +184,7 @@ class Hamiltonian():
 
         # Sort descending order of the number representation
         sorted_order = np.argsort([eval(x) for x in self.base])[::-1]
-        B = self.reorder_base(sorted_order)
+        self.reorder_base(sorted_order)
 
 
     def reorder_base(self, new_order: List[int]) -> Matrix:
@@ -173,9 +289,28 @@ class CEF_Hamiltonian(Hamiltonian):
         super().__init__(base=freeionkets, matrix=matrix_representation)
 
         # Other class specific fields
-        self.subs = None
+        self.subs = []
 
+    def determine_eigenvalues(self, from_blocks: bool=True):
+        if not from_blocks:
+            return super().determine_eigenvalues()
+        
+        if not self.subs:
+            self.make_block_form()
 
+        # This approach looses information about the subspace origin
+        # of the eigenvalue.
+        eigenvalues_dict = {}
+        for Hsub in self.subs:
+            for eigenvalue, degeneracy in Hsub.determine_eigenvalues():
+                if eigenvalue in eigenvalues_dict:
+                    eigenvalues_dict[eigenvalue] += degeneracy
+                else:
+                    eigenvalues_dict[eigenvalue] = degeneracy
+
+        self.eigenvalues = [(ev, deg) for ev,deg in eigenvalues_dict.items()]
+
+        return self.eigenvalues
 
 
     # Methods for making the Jordan form of the Hamiltonian
---
+++ data_containers.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def sympy_expression_mapping():
+    return {'sqrt':np.emath.sqrt, 'I':1j}
+
+# class CEF_tables():
+# 	pass
+# 	'''
+# 	Hard-coded data on various CEF related objects, printing and conversion functionalities.
+#
+# 	'''
+# 	def __init__(self):
+# 		return
+
+# 	def fill_symmetry(self):
+# 		return
